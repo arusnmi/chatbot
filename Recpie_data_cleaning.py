cd3c7765f8d35e6d8cc7e93c47289bc22891fdae
arusnmi <122535265+arusnmi@users.noreply.github.com>
2025-04-24 04:25:26 +0000

    did the inventory csv

--- Recpie_data_cleaning.py
+++ Recpie_data_cleaning.py
@@ -5,11 +5,9 @@ import random
 dataframe = pd.read_csv('indian_food.csv')
 
 
-sorted_df = dataframe.drop(columns=['state', 'region'])
 
 
-recpies=pd.DataFrame(sorted_df)
+recpies=pd.DataFrame(dataframe)
 
-recpies["quantity"]= random.randint(100, 1000, size=len(recpies))
 
 recpies.to_csv('recpies.csv', index=False)
